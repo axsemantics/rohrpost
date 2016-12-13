331503e3f89ddeef840820bb9ebee82e1d623fd4
Tobias Kunze <rixx@cutebit.de>
2016-12-13 16:51:05 +0100

    Raise exceptions on malformed messages

--- rohrpost/message.py
+++ rohrpost/message.py
@@ -16,10 +16,15 @@ def send_message(message, message_id, handler, close=False, **additional_data):
         content['type'] = handler
 
     content.update(**additional_data)
+    if not content:
+        raise Exception('Cannot send an empty message.')
     _send_message(message, content, close=close)
 
 
 def send_success(message, message_id, handler, close=False, **additional_data):
+    if not message_id or not handler:
+        raise Exception('You have to provide a message ID and handler on success messages.')
+
     send_message(message, message_id, handler, close=close, success=True, **additional_data)
 
 
