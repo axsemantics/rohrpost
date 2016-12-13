695f4a2ae080d7d64d156ba56dc99ddaef3afcad
Tobias Kunze <rixx@cutebit.de>
2016-12-13 16:51:30 +0100

    Add test fixtures to simulate responses

---
+++ tests/conftest.py
@@ -0,0 +1,24 @@
+import json
+
+import pytest
+
+
+class ReplyChannel:
+    def __init__(self):
+        self.data = []
+        self.closed = False
+
+    def send(self, message_dict):
+        if not self.closed:
+            self.data.append(json.loads(message_dict.get('text')))
+            self.closed = message_dict.get('close', False)
+
+
+class Message:
+    def __init__(self):
+        self.reply_channel = ReplyChannel()
+
+
+@pytest.fixture
+def message():
+    return Message()
