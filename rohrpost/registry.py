929c90460f341c17adfc1c45b371d5e15c5556d4
Tobias Kunze <rixx@cutebit.de>
2016-12-13 15:45:47 +0100

    Add the rohrpost registry
    It's a normal wrapper! It does not work as @rohrpost_handler (instead of
    @rohrpost_handler()) yet, but it can choose the method name if no
    handler name was given.

---
+++ rohrpost/registry.py
@@ -0,0 +1,26 @@
+HANDLERS = dict()
+
+
+def _rohrpost_handler(func, name):
+    def ret_func(*args, **kwargs):
+        return func(*args, **kwargs)
+
+    def update_registry(key, value):
+        if key in HANDLERS:
+            raise Exception('Handler for "{}" is already defined.'.format(key))
+        HANDLERS[key] = value
+
+    name = name or func.__name__
+
+    if isinstance(name, list):
+        for n in name:
+            update_registry(n, func)
+    else:
+        update_registry(name, func)
+    return ret_func
+
+
+def rohrpost_handler(name=''):
+    def wrap(f):
+        return _rohrpost_handler(f, name)
+    return wrap
