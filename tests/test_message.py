f76e1c7f24f4d87f1028fe6fb1605808592cb0b7
Tobias Kunze <rixx@cutebit.de>
2016-12-13 16:52:57 +0100

    Test error messages

--- tests/test_message.py
+++ tests/test_message.py
@@ -53,3 +53,40 @@ def test_send_error_without_anything(message):
     assert 'id' not in data[0]
     assert 'type' not in data[0]
     assert data[0]['error'] == 'error_message'
+
+
+def test_send_success(message):
+    send_success(message, 1, 'some_handler')
+    data = message.reply_channel.data
+    assert len(data) == 1
+    assert data[0]['id'] == 1
+    assert data[0]['type'] == 'some_handler'
+    assert data[0]['success'] is True
+
+
+def test_send_success_with_additional_data(message):
+    send_success(message, 1, 'some_handler', some_field='additional_info')
+    data = message.reply_channel.data
+    assert len(data) == 1
+    assert data[0]['id'] == 1
+    assert data[0]['type'] == 'some_handler'
+    assert data[0]['success'] is True
+    assert data[0]['some_field'] == 'additional_info'
+
+
+def test_send_success_without_id(message):
+    with pytest.raises(Exception) as exc:
+        send_success(message, None, 'some_handler')
+    assert 'ID and handler' in str(exc.value)
+
+
+def test_send_success_without_id_with_additional_data(message):
+    with pytest.raises(Exception) as exc:
+        send_success(message, None, 'some_handler', 'success_message', some_field='additional_info')
+    assert 'ID and handler' in str(exc.value)
+
+
+def test_send_success_without_id_and_type(message):
+    with pytest.raises(Exception) as exc:
+        send_success(message, None, None, 'success_message', some_field='additional_info')
+    assert 'ID and handler' in str(exc.value)
