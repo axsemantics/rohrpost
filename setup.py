ba4a5a014082d87789268d0962c1d8ee1f5a080e
Tobias Kunze <rixx@cutebit.de>
2016-12-15 14:59:47 +0100

    Initial PyPI config

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name = 'rohrpost',
+    version = '0.0.0',
+
+    description = 'rohrpost WebSocket protocol for ASGI',
+
+    url = 'https://github.com/axsemantics/rohrpost',
+
+    author = 'Tobias Kunze',
+    author_email = 'tobias.kunze@ax-semantics.com',
+
+    license = 'MIT',
+
+    classifiers = [
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    packages = ['rohrpost'],
+)
