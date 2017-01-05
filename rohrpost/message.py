d5bcfe8734ebb0ae360c3922066ef3065a8d2fff
Tobias Kunze <rixx@cutebit.de>
2017-01-05 14:48:28 +0100

    Make error a top level field and remove the success field

--- rohrpost/message.py
+++ rohrpost/message.py
@@ -8,12 +8,16 @@ def _send_message(message, content: dict, close: bool):
     })
 
 
-def send_message(message, message_id, handler, close=False, **additional_data):
+def send_message(message, message_id, handler, close=False, error=None, **additional_data):
     content = dict()
     if message_id:
         content['id'] = message_id
     if handler:
         content['type'] = handler
+    if error:
+        content['error'] = error
+    if additional_data:
+        content['data'] = additional_data
 
     content.update(**additional_data)
     if not content:
@@ -22,12 +26,17 @@ def send_message(message, message_id, handler, close=False, **additional_data):
 
 
 def send_success(message, message_id, handler, close=False, **additional_data):
+    """
+    This method directly wraps send_message but checks the existence of id and type.
+    """
     if not message_id or not handler:
         raise Exception('You have to provide a message ID and handler on success messages.')
 
-    send_message(message, message_id, handler, close=close, success=True, **additional_data)
+    send_message(message, message_id, handler, close=close, **additional_data)
 
 
 def send_error(message, message_id, handler, error, close=False, **additional_data):
-    additional_data.update({'error': error})
-    send_message(message, message_id, handler, close=close, **additional_data)
+    """
+    This method wraps send_message and makes sure that error is a keyword argument.
+    """
+    send_message(message, message_id, handler, close=close, error=error, **additional_data)
