403a1353bb43306314f37ab6c468b284c8d80ab6
Tobias Kunze <rixx@cutebit.de>
2017-01-05 11:05:42 +0100

    First working push mixin implementation
    Doesn't yet use the proper message format

--- rohrpost/mixins.py
+++ rohrpost/mixins.py
@@ -1,3 +1,6 @@
+import json
+
+
 class PushNotificationOnChangeModelMixin:
     """
     This model mixin class sends rohrpost messages every time an object is created, updated, or
@@ -20,4 +23,40 @@ class PushNotificationOnChangeModelMixin:
         }
     }
     """
-    pass
+    def _get_group_name(self):
+        if hasattr(self, "get_group_name"):
+            return self.get_group_name()
+        if hasattr(self, "group_name"):
+            return self.group_name.format(pk=self.pk)
+        return '{class_name}-{pk}'.format(
+            class_name=self.__class__.__name__.lower(),
+            pk=self.pk,
+        )
+
+    def _get_push_data(self):
+        if hasattr(self, 'serializer_class'):
+            obj_data = self.serializer_class(self).data
+        else:
+            obj_data = {'id': self.pk}
+        if hasattr(self, 'get_push_notification_data'):
+            obj_data.update(self.get_push_notification_data())
+        return obj_data
+
+    def _send_notify(self, message_type):
+        from channels import Group
+        group_name = self._get_group_name()
+        message = self._get_push_data()
+        message['type'] = message_type
+        Group(group_name).send({
+            'text': json.dumps(message)
+        })
+
+    def save(self, *args, **kwargs):
+        message_type = 'update' if self.pk else 'create'
+        ret = super().save(self, *args, **kwargs)
+        self._send_notify(message_type)
+        return ret
+
+    def delete(self, *args, **kwargs):
+        self._send_notify('delete')
+        return super().delete(*args, **kwargs)
