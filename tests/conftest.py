17d91781a2a844a675f67275312f224c38075c97
Tobias Kunze <rixx@cutebit.de>
2017-01-09 13:10:57 +0100

    [tests] Add test model classes without using Django
    Uses mixin for full combination capabilites between features

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,8 @@ import json
 
 import pytest
 
+from rohrpost.mixins import PushNotificationOnChangeModelMixin
+
 
 class ReplyChannel:
     def __init__(self):
@@ -22,3 +24,75 @@ class Message:
 @pytest.fixture
 def message():
     return Message()
+
+
+class MockModel:
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.pk = 1
+        return self
+
+    def delete(self, *args, **kwargs):
+        pass
+
+
+class PlainExampleModel(PushNotificationOnChangeModelMixin, MockModel):
+    def __init__(self):
+        self.pk = None
+        self.name = 'test object name'
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        super().delete(*args, **kwargs)
+
+
+class ModelWithAttrMixin:
+    group_name = 'attribute-example-{pk}'
+
+
+class ModelWithMethodMixin:
+    def get_group_name(self):
+        return 'method-example-{self.pk}'.format(self=self)
+
+
+class ModelWithDataMixin:
+    def get_push_notification_data(self):
+        return {
+            'extra_name': self.name,
+            'extra_name_backwards': self.name[::-1],
+        }
+
+
+@pytest.fixture
+def plain_obj():
+    return PlainExampleModel()
+
+
+@pytest.fixture
+def obj_with_attr():
+    class ModelWithAttr(ModelWithAttrMixin, PlainExampleModel):
+        pass
+    return ModelWithAttr()
+
+
+@pytest.fixture
+def obj_with_method():
+    class ModelWithMethod(ModelWithMethodMixin, PlainExampleModel):
+        pass
+    return ModelWithMethod()
+
+
+@pytest.fixture
+def obj_with_method_and_attr():
+    class ModelWithMethodAndAttr(ModelWithMethodMixin, ModelWithAttrMixin, PlainExampleModel):
+        pass
+    return ModelWithMethodAndAttr()
+
+
+@pytest.fixture
+def obj_with_data():
+    class ModelWithData(ModelWithDataMixin, PlainExampleModel):
+        pass
+    return ModelWithData()
