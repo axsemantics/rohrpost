817007badc15e84a74c75060b821b3080b14bfa0
Tobias Kunze <rixx@cutebit.de>
2017-01-09 13:46:52 +0100

    [tests] Test subscription response types

--- tests/test_mixins.py
+++ tests/test_mixins.py
@@ -22,18 +22,21 @@ def test_without_name(plain_obj, monkeypatch):
     assert len(LOGGED_DATA['plainexamplemodel-1']) == 1
     assert LOGGED_DATA['plainexamplemodel-1'][-1]['type'] == 'subscription-update'
     assert LOGGED_DATA['plainexamplemodel-1'][-1]['data']['id'] == 1
+    assert LOGGED_DATA['plainexamplemodel-1'][-1]['data']['type'] == 'create'
 
     # update
     plain_obj.save()
     assert len(LOGGED_DATA['plainexamplemodel-1']) == 2
     assert LOGGED_DATA['plainexamplemodel-1'][-1]['type'] == 'subscription-update'
     assert LOGGED_DATA['plainexamplemodel-1'][-1]['data']['id'] == 1
+    assert LOGGED_DATA['plainexamplemodel-1'][-1]['data']['type'] == 'update'
 
     # delete
     plain_obj.delete()
     assert len(LOGGED_DATA['plainexamplemodel-1']) == 3
     assert LOGGED_DATA['plainexamplemodel-1'][-1]['type'] == 'subscription-update'
     assert LOGGED_DATA['plainexamplemodel-1'][-1]['data']['id'] == 1
+    assert LOGGED_DATA['plainexamplemodel-1'][-1]['data']['type'] == 'delete'
     LOGGED_DATA['plainexamplemodel-1'] = []
 
 
@@ -45,18 +48,21 @@ def test_with_attribute_name(obj_with_attr, monkeypatch):
     assert len(LOGGED_DATA['attribute-example-1']) == 1
     assert LOGGED_DATA['attribute-example-1'][-1]['type'] == 'subscription-update'
     assert LOGGED_DATA['attribute-example-1'][-1]['data']['id'] == 1
+    assert LOGGED_DATA['attribute-example-1'][-1]['data']['type'] == 'create'
 
     # update
     obj_with_attr.save()
     assert len(LOGGED_DATA['attribute-example-1']) == 2
     assert LOGGED_DATA['attribute-example-1'][-1]['type'] == 'subscription-update'
     assert LOGGED_DATA['attribute-example-1'][-1]['data']['id'] == 1
+    assert LOGGED_DATA['attribute-example-1'][-1]['data']['type'] == 'update'
 
     # delete
     obj_with_attr.delete()
     assert len(LOGGED_DATA['attribute-example-1']) == 3
     assert LOGGED_DATA['attribute-example-1'][-1]['type'] == 'subscription-update'
     assert LOGGED_DATA['attribute-example-1'][-1]['data']['id'] == 1
+    assert LOGGED_DATA['attribute-example-1'][-1]['data']['type'] == 'delete'
     LOGGED_DATA['attribute-example-1'] = []
 
 
@@ -68,18 +74,21 @@ def test_with_method_name(obj_with_method, monkeypatch):
     assert len(LOGGED_DATA['method-example-1']) == 1
     assert LOGGED_DATA['method-example-1'][-1]['type'] == 'subscription-update'
     assert LOGGED_DATA['method-example-1'][-1]['data']['id'] == 1
+    assert LOGGED_DATA['method-example-1'][-1]['data']['type'] == 'create'
 
     # update
     obj_with_method.save()
     assert len(LOGGED_DATA['method-example-1']) == 2
     assert LOGGED_DATA['method-example-1'][-1]['type'] == 'subscription-update'
     assert LOGGED_DATA['method-example-1'][-1]['data']['id'] == 1
+    assert LOGGED_DATA['method-example-1'][-1]['data']['type'] == 'update'
 
     # delete
     obj_with_method.delete()
     assert len(LOGGED_DATA['method-example-1']) == 3
     assert LOGGED_DATA['method-example-1'][-1]['type'] == 'subscription-update'
     assert LOGGED_DATA['method-example-1'][-1]['data']['id'] == 1
+    assert LOGGED_DATA['method-example-1'][-1]['data']['type'] == 'delete'
     LOGGED_DATA['method-example-1'] = []
 
 
@@ -91,18 +100,21 @@ def test_with_method_and_attr(obj_with_method_and_attr, monkeypatch):
     assert len(LOGGED_DATA['method-example-1']) == 1
     assert LOGGED_DATA['method-example-1'][-1]['type'] == 'subscription-update'
     assert LOGGED_DATA['method-example-1'][-1]['data']['id'] == 1
+    assert LOGGED_DATA['method-example-1'][-1]['data']['type'] == 'create'
 
     # update
     obj_with_method_and_attr.save()
     assert len(LOGGED_DATA['method-example-1']) == 2
     assert LOGGED_DATA['method-example-1'][-1]['type'] == 'subscription-update'
     assert LOGGED_DATA['method-example-1'][-1]['data']['id'] == 1
+    assert LOGGED_DATA['method-example-1'][-1]['data']['type'] == 'update'
 
     # delete
     obj_with_method_and_attr.delete()
     assert len(LOGGED_DATA['method-example-1']) == 3
     assert LOGGED_DATA['method-example-1'][-1]['type'] == 'subscription-update'
     assert LOGGED_DATA['method-example-1'][-1]['data']['id'] == 1
+    assert LOGGED_DATA['method-example-1'][-1]['data']['type'] == 'delete'
     LOGGED_DATA['method-example-1'] = []
 
 
@@ -114,18 +126,21 @@ def test_with_additional_data(obj_with_data, monkeypatch):
     assert len(LOGGED_DATA['modelwithdata-1']) == 1
     assert LOGGED_DATA['modelwithdata-1'][-1]['type'] == 'subscription-update'
     assert LOGGED_DATA['modelwithdata-1'][-1]['data']['id'] == 1
+    assert LOGGED_DATA['modelwithdata-1'][-1]['data']['type'] == 'create'
 
     # update
     obj_with_data.save()
     assert len(LOGGED_DATA['modelwithdata-1']) == 2
     assert LOGGED_DATA['modelwithdata-1'][-1]['type'] == 'subscription-update'
     assert LOGGED_DATA['modelwithdata-1'][-1]['data']['id'] == 1
+    assert LOGGED_DATA['modelwithdata-1'][-1]['data']['type'] == 'update'
 
     # delete
     obj_with_data.delete()
     assert len(LOGGED_DATA['modelwithdata-1']) == 3
     assert LOGGED_DATA['modelwithdata-1'][-1]['type'] == 'subscription-update'
     assert LOGGED_DATA['modelwithdata-1'][-1]['data']['id'] == 1
+    assert LOGGED_DATA['modelwithdata-1'][-1]['data']['type'] == 'delete'
     LOGGED_DATA['modelwithdata-1'] = []
 
 
@@ -138,18 +153,21 @@ def test_with_serializer(obj_with_serializer, monkeypatch):
     assert len(LOGGED_DATA['modelwithserializer-1']) == 1
     assert LOGGED_DATA['modelwithserializer-1'][-1]['type'] == 'subscription-update'
     assert LOGGED_DATA['modelwithserializer-1'][-1]['data']['serialized_id'] == 1
+    assert LOGGED_DATA['modelwithserializer-1'][-1]['data']['type'] == 'create'
 
     # update
     obj_with_serializer.save()
     assert len(LOGGED_DATA['modelwithserializer-1']) == 2
     assert LOGGED_DATA['modelwithserializer-1'][-1]['type'] == 'subscription-update'
     assert LOGGED_DATA['modelwithserializer-1'][-1]['data']['serialized_id'] == 1
+    assert LOGGED_DATA['modelwithserializer-1'][-1]['data']['type'] == 'update'
 
     # delete
     obj_with_serializer.delete()
     assert len(LOGGED_DATA['modelwithserializer-1']) == 3
     assert LOGGED_DATA['modelwithserializer-1'][-1]['type'] == 'subscription-update'
     assert LOGGED_DATA['modelwithserializer-1'][-1]['data']['serialized_id'] == 1
+    assert LOGGED_DATA['modelwithserializer-1'][-1]['data']['type'] == 'delete'
     LOGGED_DATA['modelwithserializer-1'] = []
 
 
@@ -161,16 +179,19 @@ def test_with_serializer_and_data(obj_with_serializer_and_data, monkeypatch):
     assert len(LOGGED_DATA['modelwithserializeranddata-1']) == 1
     assert LOGGED_DATA['modelwithserializeranddata-1'][-1]['type'] == 'subscription-update'
     assert LOGGED_DATA['modelwithserializeranddata-1'][-1]['data']['serialized_id'] == 1
+    assert LOGGED_DATA['modelwithserializeranddata-1'][-1]['data']['type'] == 'create'
 
     # update
     obj_with_serializer_and_data.save()
     assert len(LOGGED_DATA['modelwithserializeranddata-1']) == 2
     assert LOGGED_DATA['modelwithserializeranddata-1'][-1]['type'] == 'subscription-update'
     assert LOGGED_DATA['modelwithserializeranddata-1'][-1]['data']['serialized_id'] == 1
+    assert LOGGED_DATA['modelwithserializeranddata-1'][-1]['data']['type'] == 'update'
 
     # delete
     obj_with_serializer_and_data.delete()
     assert len(LOGGED_DATA['modelwithserializeranddata-1']) == 3
     assert LOGGED_DATA['modelwithserializeranddata-1'][-1]['type'] == 'subscription-update'
     assert LOGGED_DATA['modelwithserializeranddata-1'][-1]['data']['serialized_id'] == 1
+    assert LOGGED_DATA['modelwithserializeranddata-1'][-1]['data']['type'] == 'delete'
     LOGGED_DATA['modelwithserializeranddata-1'] = []
