2902d16c4154dfaa5962be906d65fc7db3ce7488
Tobias Kunze <rixx@cutebit.de>
2017-01-10 16:05:22 +0100

    [tests] Test ping/pong

---
+++ tests/test_ping.py
@@ -0,0 +1,42 @@
+from rohrpost.handlers import handle_ping
+
+
+def test_ping(message):
+    handle_ping(message, request={'id': 123})
+    assert message.reply_channel.closed is False
+    assert len(message.reply_channel.data) == 1
+
+    data = message.reply_channel.data[-1]
+    assert data['id'] == 123
+    assert data['type'] == 'pong'
+    assert not 'data' in data
+
+
+def test_ping_additional_data(message):
+    handle_ping(message, request={
+        'id': 123,
+        'type': 'ping',
+        'data': {'some': 'data', 'other': 'data'}
+    })
+    assert message.reply_channel.closed is False
+    assert len(message.reply_channel.data) == 1
+
+    data = message.reply_channel.data[-1]
+    assert data['id'] == 123
+    assert data['type'] == 'pong'
+    assert data['data']['some'] == 'data'
+
+
+def test_ping_additional_non_dict_data(message):
+    handle_ping(message, request={
+        'id': 123,
+        'type': 'ping',
+        'data': 1
+    })
+    assert message.reply_channel.closed is False
+    assert len(message.reply_channel.data) == 1
+
+    data = message.reply_channel.data[-1]
+    assert data['id'] == 123
+    assert data['type'] == 'pong'
+    assert data['data']['data'] == 1
