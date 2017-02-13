cea19b6243717bfb648fd1fa3a315e3badf03fce
Tobias Kunze <rixx@cutebit.de>
2017-02-13 16:41:29 +0100

    [mixins] Separate Create/Update/Delete mixin

--- rohrpost/mixins.py
+++ rohrpost/mixins.py
@@ -9,35 +9,8 @@ except:
     print('Channels is not installed, running in test mode!')
 
 
-class NotifyOnChange:
-    """
-    This model mixin class sends rohrpost messages every time an object is created, updated, or
-    deleted, to the corresponding group. The group is named, most important first:
-        - As the result of get_group_name() if the object has such a method
-        - As the result of group_name.format(pk=object.pk) if the object/class has a group_name
-          attribute
-        - As the result of '{class_name}-{pk}'.format(class_name=object.__class__.__name__.lower()
-          pk=object.pk) otherwise
-    The serialized object will be taken either from:
-        - The result of get_push_notification_data(updated_fields=None) if such a method exists, else
-        - The result of serializer_class(self).data, if such an attribute exists, else
-        - {"id": self.pk}
-    The serialized object will also contain an 'updated_fields' attribute with a list *if*
-    'updated_fields' was not set by the get_push_notification_data() or the serializer_class
-    *and* if it was set when calling the save() method.
-    get_push_notification_data() receives a list of updated_fields.
+class NotifyBase:
 
-    The message will look like this:
-    {
-        "id": <some id>,
-        "type": "subscription-update",
-        "data": {
-            "type": <create|update|delete>,
-            "group": <group-name>,
-            "object": <serialized object>,
-        }
-    }
-    """
     def _get_group_name(self):
         if hasattr(self, "get_group_name"):
             return self.get_group_name()
@@ -75,12 +48,61 @@ class NotifyOnChange:
             ))
         })
 
+
+class NotifyOnCreate(NotifyBase):
+
     def save(self, *args, **kwargs):
-        message_type = 'update' if self.pk else 'create'
+        initial_pk = self.pk
         ret = super().save(*args, **kwargs)
-        self._send_notify(message_type, updated_fields=kwargs.get('update_fields'))
+        if not initial_pk:
+            self._send_notify('create', updated_fields=kwargs.get('update_fields'))
         return ret
+
+
+class NotifyOnUpdate(NotifyBase):
+
+    def save(self, *args, **kwargs):
+        initial_pk = self.pk
+        ret = super().save(*args, **kwargs)
+        if initial_pk:
+            self._send_notify('update', updated_fields=kwargs.get('update_fields'))
+        return ret
+
+
+class NotifyOnDelete(NotifyBase):
 
     def delete(self, *args, **kwargs):
         self._send_notify('delete')
         return super().delete(*args, **kwargs)
+
+
+class NotifyOnChange(NotifyOnCreate, NotifyOnUpdate, NotifyOnDelete):
+    """
+    This model mixin class sends rohrpost messages every time an object is created, updated, or
+    deleted, to the corresponding group. The group is named, most important first:
+        - As the result of get_group_name() if the object has such a method
+        - As the result of group_name.format(pk=object.pk) if the object/class has a group_name
+          attribute
+        - As the result of '{class_name}-{pk}'.format(class_name=object.__class__.__name__.lower()
+          pk=object.pk) otherwise
+    The serialized object will be taken either from:
+        - The result of get_push_notification_data(updated_fields=None) if such a method exists, else
+        - The result of serializer_class(self).data, if such an attribute exists, else
+        - {"id": self.pk}
+    The serialized object will also contain an 'updated_fields' attribute with a list *if*
+    'updated_fields' was not set by the get_push_notification_data() or the serializer_class
+    *and* if it was set when calling the save() method.
+    get_push_notification_data() receives a list of updated_fields.
+
+    The message will look like this:
+    {
+        "id": <some id>,
+        "type": "subscription-update",
+        "data": {
+            "type": <create|update|delete>,
+            "group": <group-name>,
+            "object": <serialized object>,
+        }
+    }
+    """
+    pass
