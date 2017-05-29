4e354a0507deaba9ca1803dcb691a4c5830c2ef1
Tobias Kunze <rixx@cutebit.de>
2017-05-29 14:34:27 +0200

    Move test classes to rohrpost.tests

---
+++ rohrpost/tests.py
@@ -0,0 +1,17 @@
+import json
+
+
+class ReplyChannel:
+    def __init__(self):
+        self.data = []
+        self.closed = False
+
+    def send(self, message_dict):
+        if not self.closed:
+            self.data.append(json.loads(message_dict.get('text')))
+            self.closed = message_dict.get('close', False)
+
+
+class Message:
+    def __init__(self):
+        self.reply_channel = ReplyChannel()
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,24 +1,7 @@
-import json
-
 import pytest
 
 from rohrpost.mixins import NotifyOnChange
-
-
-class ReplyChannel:
-    def __init__(self):
-        self.data = []
-        self.closed = False
-
-    def send(self, message_dict):
-        if not self.closed:
-            self.data.append(json.loads(message_dict.get('text')))
-            self.closed = message_dict.get('close', False)
-
-
-class Message:
-    def __init__(self):
-        self.reply_channel = ReplyChannel()
+from rohrpost.tests import Message
 
 
 @pytest.fixture
