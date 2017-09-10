44ba1faf417b79a7110fc5a87978ab7d10c9491c
Tobias Kunze <rixx@cutebit.de>
2017-09-10 13:46:14 +0200

    Enforce keyword arguments in helpers

--- rohrpost/message.py
+++ rohrpost/message.py
@@ -2,14 +2,14 @@ import json
 import random
 
 
-def _send_message(message, content: dict, close: bool):
+def _send_message(*, message, content: dict, close: bool):
     message.reply_channel.send({
         'text': json.dumps(content),
         'close': close,
     })
 
 
-def build_message(handler, message_id=None, error=None, generate_id=False, **additional_data):
+def build_message(*, handler, message_id=None, error=None, generate_id=False, **additional_data):
     content = dict()
     if message_id:
         content['id'] = message_id
@@ -25,26 +25,26 @@ def build_message(handler, message_id=None, error=None, generate_id=False, **add
     return content
 
 
-def send_message(message, message_id, handler, close=False, error=None, **additional_data):
+def send_message(*, message, handler, message_id=None, close=False, error=None, **additional_data):
     content = build_message(handler=handler, message_id=message_id, error=error, **additional_data)
 
     if not content:
         raise Exception('Cannot send an empty message.')
-    _send_message(message, content, close=close)
+    _send_message(message=message, content=content, close=close)
 
 
-def send_success(message, message_id, handler, close=False, **additional_data):
+def send_success(*, message, handler, message_id, close=False, **additional_data):
     """
     This method directly wraps send_message but checks the existence of id and type.
     """
     if message_id is None or handler is None:
         raise Exception('You have to provide a message ID and handler on success messages.')
 
-    send_message(message, message_id, handler, close=close, **additional_data)
+    send_message(message=message, message_id=message_id, handler=handler, close=close, **additional_data)
 
 
-def send_error(message, message_id, handler, error, close=False, **additional_data):
+def send_error(*, message, handler, message_id, error, close=False, **additional_data):
     """
     This method wraps send_message and makes sure that error is a keyword argument.
     """
-    send_message(message, message_id, handler, close=close, error=error, **additional_data)
+    send_message(message=message, message_id=message_id, handler=handler, error=error, close=close, **additional_data)
