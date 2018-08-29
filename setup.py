4a7663365ae01f69a33a83bffa3859ff90df127f
Michael Käufl <aexea@c.michael-kaeufl.de>
2018-08-29 16:01:02 +0200

    Add more classifiers

--- setup.py
+++ setup.py
@@ -19,12 +19,17 @@ setup(
     license="MIT",
     python_requires=">=3.4",
     classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Topic :: Internet :: WWW/HTTP",
     ],
     packages=["rohrpost"],
 )
