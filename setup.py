bf0cd05a447c6e4d59a5924abcd894f23ccf05fa
Michael Käufl <aexea@c.michael-kaeufl.de>
2018-08-29 13:09:36 +0200

    Format setup.py with black

--- setup.py
+++ setup.py
@@ -3,27 +3,20 @@ from setuptools import setup
 from rohrpost import __version__
 
 setup(
-    name = 'rohrpost',
-    version = __version__,
-
-    description = 'rohrpost WebSocket protocol for ASGI',
-
-    url = 'https://github.com/axsemantics/rohrpost',
-
-    author = 'Tobias Kunze',
-    author_email = 'tobias.kunze@ax-semantics.com',
-
-    license = 'MIT',
-
-    classifiers = [
-        'License :: OSI Approved :: MIT License',
-
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+    name="rohrpost",
+    version=__version__,
+    description="rohrpost WebSocket protocol for ASGI",
+    url="https://github.com/axsemantics/rohrpost",
+    author="Tobias Kunze",
+    author_email="tobias.kunze@ax-semantics.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
-
-    packages = ['rohrpost'],
+    packages=["rohrpost"],
 )
