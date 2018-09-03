d2ca006957b45c9bd32c02c2182f1b8ec166c54b
Michael Käufl <aexea@c.michael-kaeufl.de>
2018-09-03 17:01:03 +0200

    [registry] Remove unnecessary wrapper

--- rohrpost/registry.py
+++ rohrpost/registry.py
@@ -3,10 +3,7 @@ from typing import Callable, Dict, List, Union
 HANDLERS: Dict[str, Callable] = dict()
 
 
-def _rohrpost_handler(func: Callable, name: Union[str, List[str]]):
-    def ret_func(*args, **kwargs):
-        return func(*args, **kwargs)
-
+def _rohrpost_handler(func: Callable, name: Union[str, List[str]]) -> Callable:
     def update_registry(key: str, value: Callable):
         if key in HANDLERS:
             raise Exception('Handler for "{}" is already defined.'.format(key))
@@ -19,10 +16,12 @@ def _rohrpost_handler(func: Callable, name: Union[str, List[str]]):
             update_registry(n, func)
     else:
         update_registry(name, func)
-    return ret_func
+    return func
 
 
-def rohrpost_handler(name: Union[str, List[str]] = ""):
+def rohrpost_handler(
+    name: Union[str, List[str]] = ""
+) -> Callable[[Callable], Callable]:
     def wrap(f):
         return _rohrpost_handler(f, name)
 
