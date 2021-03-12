b3f71ac5acede26fb15fd7ead6971259b8ba1bb3
Michael Käufl <aexea@c.michael-kaeufl.de>
2021-03-12 13:37:38 +0100

    Switch from setup.py to setup.cfg (declarative)

--- rohrpost/__init__.py
+++ rohrpost/__init__.py
@@ -1,2 +1,11 @@
-__version__ = "2.0.1"
-VERSION = __version__
+import pkg_resources  # part of setuptools
+
+try:
+    dist = pkg_resources.get_distribution("rohrpost")
+except pkg_resources.DistributionNotFound:
+    # This happens when the package isn't installed
+    __version__: str = "0.0.0"
+else:
+    __version__ = dist.version
+
+VERSION: str = __version__
--- setup.py
+++ setup.py
@@ -1,42 +1,5 @@
-from setuptools import setup  # type: ignore[import]
+#!/usr/bin/env python
 
-from rohrpost import __version__
+import setuptools
 
-
-def read(filepath: str) -> str:
-    with open(filepath, "r", encoding="utf-8") as f:
-        return f.read()
-
-
-setup(
-    name="rohrpost",
-    version=__version__,
-    description="rohrpost WebSocket protocol for ASGI",
-    long_description=read("README.rst"),
-    url="https://github.com/axsemantics/rohrpost",
-    author="Tobias Kunze",
-    author_email="tobias.kunze@ax-semantics.com",
-    license="MIT",
-    python_requires=">=3.6",
-    install_requires=["channels>=3.0"],
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Web Environment",
-        "Intended Audience :: Developers",
-        "Operating System :: OS Independent",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3 :: Only",
-        "Topic :: Internet :: WWW/HTTP",
-    ],
-    packages=["rohrpost"],
-    project_urls={
-        "Documentation": "https://rohrpost.readthedocs.io/en/stable/",
-        "Source": "https://github.com/axsemantics/rohrpost",
-        "Tracker": "https://github.com/axsemantics/rohrpost/issues",
-    },
-)
+setuptools.setup()
