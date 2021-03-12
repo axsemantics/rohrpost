d998f632b91d0cc3d10ae0ea9ae4eb15613667a6
Michael Käufl <aexea@c.michael-kaeufl.de>
2021-03-12 14:46:29 +0100

    Run mypy only against ./rohrpost

--- rohrpost/tests.py
+++ rohrpost/tests.py
@@ -1,14 +1,15 @@
 import json
+from typing import Any, List
 
 
 class Consumer:
-    def __init__(self):
-        self.data = []
+    def __init__(self) -> None:
+        self.data: List[Any] = []
         self.closed = False
 
-    def send(self, message: str):
+    def send(self, message: str) -> None:
         if not self.closed:
             self.data.append(json.loads(message))
 
-    def close(self):
+    def close(self) -> None:
         self.closed = True
