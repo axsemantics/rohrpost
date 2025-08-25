3dfd3a59155a905047eb05ae500a514e008bfdcb
Michael Käufl <aexea@c.michael-kaeufl.de>
2025-08-25 16:00:28 +0200

    Remove unused script

--- check_version.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-VERSION="$(grep -oP -m1 "__version__\s=\s['\"]\K[^'\"]+" rohrpost/__init__.py)"
-
-if [ "${TRAVIS_TAG}" != "v${VERSION}" ]; then
-  echo "Tag (${TRAVIS_TAG}) and version (${VERSION}) do not match."
-  exit 1
-fi
